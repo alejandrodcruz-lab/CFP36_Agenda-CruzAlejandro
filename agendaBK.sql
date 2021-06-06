a00d30040fd2d838ca9e276625233c7f9c12fd10
Alejandro Cruz <alejandrodavidcruz@gmail.com>
2021-06-05 23:46:53 -0300

    Agregado Backup

---
+++ agendaBK.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_agenda
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contactos`
+--
+
+DROP TABLE IF EXISTS `contactos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contactos` (
+  `ID_CONTACTO` smallint NOT NULL AUTO_INCREMENT,
+  `NOMBRE` varchar(40) DEFAULT NULL,
+  `APELLIDO` varchar(40) DEFAULT NULL,
+  `FECHA_NACIMIENTO` date DEFAULT NULL,
+  `EMAIL` varchar(40) DEFAULT NULL,
+  `TELEFONO` int DEFAULT NULL,
+  PRIMARY KEY (`ID_CONTACTO`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactos`
+--
+
+LOCK TABLES `contactos` WRITE;
+/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
+INSERT INTO `contactos` VALUES (1,'ALEJANDRO','CRUZ','1990-01-01','ALEJANDROCRUZ@MAIL.COM',1156325589),(2,'ELSA','PATO','1992-01-01','ELSAPATO@MAIL.COM',1156325478),(3,'ELVIS','TEK','1985-01-01','ELVISTEK@MAIL.COM',1156325369),(4,'ARMANDO','PAREDES','1980-01-01','AMRANDOPAREDES@MAIL.COM',1156325258),(5,'LUZ','ROJAS','1997-01-01','LUZROJAS@MAIL.COM',1156325789);
+/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contactos_hist`
+--
+
+DROP TABLE IF EXISTS `contactos_hist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contactos_hist` (
+  `ID_HIST` smallint NOT NULL AUTO_INCREMENT,
+  `NOMBRE` varchar(40) DEFAULT NULL,
+  `APELLIDO` varchar(40) DEFAULT NULL,
+  `FECHA_NACIMIENTO` date DEFAULT NULL,
+  `EMAIL` varchar(40) DEFAULT NULL,
+  `TELEFONO` int DEFAULT NULL,
+  `ID_ORIGINAL` smallint DEFAULT NULL,
+  PRIMARY KEY (`ID_HIST`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactos_hist`
+--
+
+LOCK TABLES `contactos_hist` WRITE;
+/*!40000 ALTER TABLE `contactos_hist` DISABLE KEYS */;
+INSERT INTO `contactos_hist` VALUES (1,'PEPE','GRILLO','1999-06-05','PEPE.GRILLO@YAHOO.COM.AR',1155664455,6);
+/*!40000 ALTER TABLE `contactos_hist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-05 23:45:40
